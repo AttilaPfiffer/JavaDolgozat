ba562fe8d36f2b9f34a0099d3b5f2b368512882f
AttilaPfiffer <pifuati@gmail.com>
2024-12-09 10:16:35 +0100

    f1 4

--- src/main/java/model/Tabla.java
+++ src/main/java/model/Tabla.java
@@ -10,8 +10,19 @@ public class Tabla {
     public Tabla(char uresCella) {
         t = new char[8][8];
         this.uresCella = uresCella;
+
     }
 
-    
-}
+    public void megjelenit() {
+        Tabla tabla = new Tabla('#');
+        for(int i = 0; i < t.length; i++) {
+            for(int j = 0; j < t.length; j++) {
+                System.out.println(t[i][j]);
+            }
+        }
+    }
 
+    public void elhelyez() {
+
+    }
+}
